946827e78d43f14e669fa7cad01acb9e37361190
SofRahmani <sofiane.rahmani12@gmail.com>
2024-04-03 19:17:20 +0200

    feat: :sparkles: setup Nextra

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import Nextra from "nextra";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withNextra = Nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+export default withNextra(nextConfig);
